4b2a21cc60471bfd11dfd587c0e420c865c28b96
petertran198 <petertran198@gmail.com>
2019-05-06 20:55:28 -0400

    Added coinmarket api, home controller , and bootstrap gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    # Get the uri 
+    @response = Net::HTTP.get(@uri)
+    #parse the json
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC","XRP","ADA","XLM","STEEM"]
   end
+
+  def about
+  end
+
+
 end
